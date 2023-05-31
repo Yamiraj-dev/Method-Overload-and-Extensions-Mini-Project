ff0feb7587960bba8c2ea7299571ee8f0c498e8f
Yamiraj <yamiraj.dev@gmail.com>
2023-06-01 05:50:36 +0800

    Made a mini project that uses method overloading and  extension methods as examples. It makes code easier to read, more compact and less dense.

--- MiniProject/Program.cs
+++ MiniProject/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace MiniProject
+﻿using System;
+
+namespace MiniProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PersonModel person = new PersonModel();
+
+           
+            person.FirstName = "What is your first name: ".RequestString();
+
+            person.LastName = "What is your last name: ".RequestString();
+
+            person.Age = "What is your last age: ".RequestInt(0, 100);
+
+            Console.ReadLine();
         }
     }
 }
